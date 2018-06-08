e314eae701565629e5953be19efa2654aed8d9bc
heavyarms2112 <heavyarms2112@gmail.com>
2018-06-07 23:11:28 -0400

    Update rigmon.sh

--- rigmon.sh
+++ rigmon.sh
@@ -9,24 +9,23 @@ Pur='\e[0;35m';     BPur='\e[1;35m';    UPur='\e[4;35m';    IPur='\e[0;95m';    
 Cya='\e[0;36m';     BCya='\e[1;36m';    UCya='\e[4;36m';    ICya='\e[0;96m';    BICya='\e[1;96m';   On_Cya='\e[46m';    On_ICya='\e[0;106m';
 Whi='\e[0;37m';     BWhi='\e[1;37m';    UWhi='\e[4;37m';    IWhi='\e[0;97m';    BIWhi='\e[1;97m';   On_Whi='\e[47m';    On_IWhi='\e[0;107m';
 
-
 #HS110 smart plug specific
-MY_SP_TOKEN="<replace-token>";
-rig1_dev_id="<dev_id1>";
-rig2_dev_id="<dev_id2>";
+MY_SP_TOKEN="NULL";
+acct_email_id=""
+acct_password=""
+#get a new UUID from website https://www.uuidgenerator.net/version4
+#This UUID will represent your Client Term ID (like the one of your Kasa App).
+acct_uuid=""
 
-#Test variables
-#TEST_DUR=600; 
-
-#General
-wait_time=60;                  #global wait time 
+#General timing in seconds
+wait_time=600;                 #global wait time
 mon_interval=5;                #power monitoring interval time
-down_time=40;
-power_thres=1250;               #power consumption threshold value
-print_interval=0;
+down_time=480;                 #down time
 
-#Rig specific
-rig1_name="top";
+power_thres=660;               #power consumption threshold value before sending out alert
+
+#Rig specific flags
+rig1_name="RIG1";
 rig1_curr_wait=0;               #rig wait time
 rig1_utime=0;                   #rig timer
 rig1_stab='N';                  #rig stabilized
@@ -36,9 +35,8 @@ rig1_down=0;
 rig1_dtime=0;
 rig1_curr_dtime=0;
 
-
-#Rig specific
-rig2_name="bot"
+#Rig specific flags
+rig2_name="RIG2"
 rig2_curr_wait=0;               #rig wait time
 rig2_utime=0;                   #rig timer
 rig2_wait=1;                    #rig stabilized
@@ -48,38 +46,68 @@ rig2_down=0;
 rig2_dtime=0;
 rig2_curr_dtime=0;
 
+#Get the rig device ids using the curl request in new_token function below
+
+#Get a new token to access your smartplug
+#curl -s --request POST "https://wap.tplinkcloud.com" --data '{"method": "login","params": {"appType": "Kasa_iOS", "cloudUserName": "'$acct_email_id'", "cloudPassword": "'$acct_password'", "terminalUUID": "'$acct_uuid'" }}' --header "Content-Type: application/json" | grep -o -P 'token.{0,35}' | cut -d'"' -f3
+
+#Use the token generated (from above query) to get the device ids using curl request below and populate rig1_dev_id and rig2_dev_id
+#curl -s --request POST "https://wap.tplinkcloud.com?token=YOUR_TOKEN_HERE HTTP/1.1" --data '{"method":"getDeviceList"}' --header "Content-Type: application/json"
+rig1_dev_id="";
+rig2_dev_id="";
 
 convertsecs() {
  ((h=${1}/3600))
  ((m=(${1}%3600)/60))
  ((s=${1}%60))
  ((d=${1}/86400))
- printf "day:%02d %02d:%02d:%02d\n" $d $h $m $s
+ printf "%02d %02d:%02d:%02d\n" $d $h $m $s
+}
+
+new_token(){
+    MY_SP_TOKEN=$(curl -s --request POST "https://wap.tplinkcloud.com" \
+    --data '{"method": "login","params": {"appType": "Kasa_iOS", "cloudUserName": "'$acct_email_id'", "cloudPassword": "'$acct_password'", "terminalUUID": "'$acct_uuid'" }}' \
+    --header "Content-Type: application/json" | grep -o -P 'token.{0,35}' | cut -d'"' -f3
+    )
+    #echo "New token is $MY_SP_TOKEN";
 }
 
 ResetFlags(){
     if [ $1 == "$rig1_name" ];then
         rig1_stab='N';
-        #rig1_pwrcycle=1;
         rig1_wait=1;
         rig1_utime=0;
         rig1_down=0;
         rig1_curr_dtime=0;
-        rig1_dtime=0;        
-    fi    
-    if [ $1 == "bot" ];then
+        rig1_dtime=0;
+    elif [ $1 == "$rig2_name" ];then
         rig2_stab='N';
-        #rig2_pwrcycle=1;
         rig2_wait=1;
         rig2_utime=0;
         rig2_down=0;
         rig2_curr_dtime=0;
-        rig2_dtime=0;        
+        rig2_dtime=0;
+    else
+        echo "Unsupported Reset";
     fi
     #echo " In ResetFlags $(convertsecs $rig1_utime) $(convertsecs $rig2_utime) ";
 }
 
-CheckDownTime(){
+Reboot(){
+    #echo " In Reboot "
+    if [ $1 == "$rig1_name" ];then
+        ResetFlags "$1"
+        #power_thres=500;
+        Notify "$1" "($rig1_power)"
+    elif [ $1 == "$rig2_name" ];then
+        ResetFlags "$1"
+        Notify "$1" "($rig2_power)"
+    else
+        echo "Unsupported Reboot";
+    fi
+}
+
+ CheckDownTime(){
     if [ $rig1_down == 1 ]; then
         rig1_curr_dtime=$rig1_dtime;
     fi
@@ -89,63 +117,46 @@ CheckDownTime(){
 
     curr_date_time=`date "+%Y-%m-%d %H:%M:%S"`;
     echo "$curr_date_time Current downtime : $(convertsecs $rig1_curr_dtime) $(convertsecs $rig2_curr_dtime)";
-    if [ $1 == "$rig1_name" ];then
+    #if [ $1 == "$rig1_name" ];then
         if [ "$rig1_curr_dtime" -ge "$down_time" ];then
-            echo "Rig $1 is down for $(convertsecs $rig1_curr_dtime). Rebooting..." >> "$log_file";
-            Reboot "$1";
+            printf "Rig $rig1_name is down for $(convertsecs $rig1_curr_dtime). Rebooting...\n" >> "$log_file";
+            Reboot "$rig1_name";
         fi;
-    fi;
-    if [ $1 == "bot" ];then        
+    #fi;
+    #if [ $1 == "$rig2_name" ];then
         if [ "$rig2_curr_dtime" -ge "$down_time" ];then
-            echo "Rig $1 is down for $(convertsecs $rig2_curr_dtime). Rebooting..." >> "$log_file";            
-            Reboot "$1";
+            printf "Rig $rig2_name is down for $(convertsecs $rig2_curr_dtime). Rebooting...\n" >> "$log_file";
+            Reboot "$rig2_name";
         fi;
-    fi;    
-}
-
-Reboot(){
-    #echo " In Reboot "
-    if [ $1 == "$rig1_name" ];then
-        ResetFlags "$1"
-        Notify "$1" "($rig1_power)"
-    fi
-    if [ $1 == "bot" ];then
-        ResetFlags "$2"
-        Notify "$1" "($rig2_power)"
-    fi
+    #fi;
 }
 
 Notify(){
     curr_date_time=`date "+%Y-%m-%d %H:%M:%S"`;
     echo "$curr_date_time power consumption below threshold value: $power_thres Watts; Rebooted Rig: $1 ($2) " >> "$log_file";
-    echo "$curr_date_time power consumption below threshold value: $power_thres Watts; Rebooted Rig: $1 ($2)" | mail -s "RigMonitor" <email_id>                                                                                                                              
+    echo "$curr_date_time power consumption below threshold value: $power_thres Watts; Rebooted Rig: $1 ($2)" | mail -s "RigMonitor" "$acct_email_id"
 }
 
 GetPower(){
     if [ $# -ne 1 ]; then
         echo "Usage: GetPower <RigName>, eg. GetPower $rig1_name"
     else
-        #printf "Running.."
-        if [ "$1" == "$rig1_name" ]; then
-            for ((i = 1; i <= 1; i++)); do
-                rig1_power=$(curl -s --request POST "https://wap.tplinkcloud.com/?token=$MY_SP_TOKEN HTTP/1.1" \
-                --data '{"method":"passthrough", "params": {"deviceId": "'$rig1_dev_id'", "requestData": "{\"system\":{\"get_sysinfo\":null},\"emeter\":{\"get_realtime\":null}}" }}' \
-                --header "Content-Type: application/json" | grep -o -P 'power.{0,10}' | cut -d':' -f2 
-                )
-            done 
-        elif [ "$1" == "bot" ]; then
-            for ((i = 1; i <= 1; i++)); do
-                rig2_power=$(curl -s --request POST "https://wap.tplinkcloud.com/?token=$MY_SP_TOKEN HTTP/1.1" \
-                --data '{"method":"passthrough", "params": {"deviceId": "'$rig2_dev_id'", "requestData": "{\"system\":{\"get_sysinfo\":null},\"emeter\":{\"get_realtime\":null}}" }}' \
-                --header "Content-Type: application/json" | grep -o -P 'power.{0,10}' | cut -d':' -f2
-                )
-            done
+        #printf "Running.. $1"
+        if [ $1 == "$rig1_name" ]; then
+            rig1_power=$(curl -s --request POST "https://wap.tplinkcloud.com/?token=$MY_SP_TOKEN HTTP/1.1" \
+            --data '{"method":"passthrough", "params": {"deviceId": "'$rig1_dev_id'", "requestData": "{\"system\":{\"get_sysinfo\":null},\"emeter\":{\"get_realtime\":null}}" }}' \
+            --header "Content-Type: application/json" | grep -o -P 'power.{0,10}' | cut -d':' -f2
+            )
+        elif [ $1 == "$rig2_name" ]; then
+            rig2_power=$(curl -s --request POST "https://wap.tplinkcloud.com/?token=$MY_SP_TOKEN HTTP/1.1" \
+            --data '{"method":"passthrough", "params": {"deviceId": "'$rig2_dev_id'", "requestData": "{\"system\":{\"get_sysinfo\":null},\"emeter\":{\"get_realtime\":null}}" }}' \
+            --header "Content-Type: application/json" | grep -o -P 'power.{0,10}' | cut -d':' -f2
+            )
         else
             echo "Rig Name: $1 is not supported";
         fi
-    fi  
+    fi
 }
-
 CheckWait(){
     rig1_curr_wait=$((rig1_curr_wait+mon_interval));
     rig2_curr_wait=$((rig2_curr_wait+mon_interval));
@@ -155,20 +166,20 @@ CheckWait(){
     fi;
     if [ $rig2_stab == 'Y' ];then
         rig2_curr_wait=0;
-    fi;       
+    fi;
     #echo "$curr_date_time Current waittime : $(convertsecs $rig1_curr_wait) $(convertsecs $rig2_curr_wait)";
     if [ $rig1_stab == 'N' ] && [ "$rig1_curr_wait" -ge "$wait_time" ];then
         rig1_curr_wait=0;
         rig1_wait=0;
-        rig1_stab='Y';     
+        rig1_stab='Y';
     fi
     if [ $rig2_stab == 'N' ] && [ "$rig2_curr_wait" -ge "$wait_time" ];then
         rig2_curr_wait=0;
         rig2_wait=0;
         rig2_stab='Y';
-    fi 
-    #echo -e "$curr_date_time Power-consumption: $rig1_name,$rig1_power ($rig1_stab) | $rig2_name,$rig2_power ($rig2_stab)";       
-        
+    fi
+    #echo -e "$curr_date_time Power-consumption: $rig1_name,$rig1_power ($rig1_stab) | $rig2_name,$rig2_power ($rig2_stab)";
+
 }
 
 countUpTime(){
@@ -181,44 +192,32 @@ countUpTime(){
 }
 
 printTime(){
-    curr_date_time=`date "+%Y-%m-%d %H:%M:%S"`;                                
-    echo -e "$curr_date_time Power-consumption: $rig1_power Watts   | $rig2_power Watts";
-    echo -e "$curr_date_time Current uptime   : $(convertsecs $rig1_utime) | $(convertsecs $rig2_utime)";    
-    echo -e "$curr_date_time Current downtime : $(convertsecs $rig1_curr_dtime) | $(convertsecs $rig2_curr_dtime)";
-    if [ $rig1_wait == 1 ] || [ $rig2_wait == 1 ];then
-    echo -e "$curr_date_time Current waittime : $(convertsecs $rig1_curr_wait) | $(convertsecs $rig2_curr_wait)";
-    fi;
-}
+     curr_date_time=`date "+%Y-%m-%d %H:%M:%S"`;
+     echo -e "[$curr_date_time] DownTime threshold: $down_time Waittime threshold: $wait_time";
+     echo -e "R1: $rig1_name($rig1_stab) $rig1_power Watts, U: $(convertsecs $rig1_utime), D: $(convertsecs $rig1_curr_dtime), W: $(convertsecs $rig1_curr_wait)";
+     echo -e "R2: $rig2_name($rig2_stab) $rig2_power Watts, U: $(convertsecs $rig2_utime), D: $(convertsecs $rig2_curr_dtime), W: $(convertsecs $rig2_curr_wait)";
+ }
 
 printStuff(){
     printf "\033c" #clear screen
-    echo -e "${Blu}"
+    echo -e "${BYel}"
     echo -e "------------------------------------------------------------------------"
-    echo -e "    RIGMON v1.0 : Auto reboot rigs based on power consumption values    "
-    echo -e "------------------------------------------------------------------------"
-    echo -e "${RCol} "
+    echo -e "|   RIGMON v1.0 : Auto reboot rigs based on power consumption values   |"
+    echo -e "------------------------------------------------------------------------""${RCol} "
     #echo "power consumption with interval $mon_interval secs after wait time of $wait_time secs";
     curr_date_time=`date "+%Y-%m-%d %H:%M:%S"`;
-    echo -e "Screen refreshes every $mon_interval seconds";
+    echo -e " screen refresh interval: $mon_interval sec, power consumption threshold: $power_thres Watts";
     echo -e "------------------------------------------------------------------------";
-    echo -e "                                       Rig1 - $rig1_name($rig1_stab)   | Rig2 - $rig2_name($rig2_stab)";
 }
-
-printStuff;
+ printStuff;
 log_file=`date "+%Y-%m-%d.log"`;
-#log_file+=".log";
-#echo "curr_date_time: Logging starts" >> "$log_file";
+new_token;
 while true
-do 
-    #CLEAR_CYCLE=$((TEST_DUR-mon_interval))
-    print_interval=$(($print_interval+1));
-    #if [ $print_interval -ge 10 ];then
-    #    print_interval=0;
-        GetPower "$rig1_name"
-        GetPower "$rig2_name"
-        printStuff;
-        printTime;
-    #fi;
+do
+    GetPower "$rig1_name"
+    GetPower "$rig2_name"
+    printStuff;
+    printTime;
     sleep $mon_interval
     countUpTime;
     if [ $rig1_wait == 1 ] || [ $rig2_wait == 1 ]; then
@@ -230,15 +229,15 @@ do
         if (( $(echo "$rig1_power < $power_thres" | bc -l) )); then
             rig1_dtime=$((rig1_dtime+mon_interval));
             rig1_down=1;
-            CheckDownTime "$rig1_name"            
+            CheckDownTime
         fi
     fi
     if [ $rig2_wait == 0 ];then
-        GetPower "bot";
+        GetPower "$rig2_name";
         if (( $(echo "$rig2_power < $power_thres" | bc -l) )); then
-            rig2_dtime=$((rig2_dtime+mon_interval));  
-            rig2_down=1;        
-            CheckDownTime "$rig2_name"            
+            rig2_dtime=$((rig2_dtime+mon_interval));
+            rig2_down=1;
+            CheckDownTime
         fi
     fi
 done
